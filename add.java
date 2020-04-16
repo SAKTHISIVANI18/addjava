3a22fc85df61df2d927e530cf92c61025c8c7ad6
SAKTHISIVANI18 <59279036+SAKTHISIVANI18@users.noreply.github.com>
2020-04-17 00:42:34 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 class Add{
    public static void main(String[] args) {
         
-      int num1 = 10, num2 = 15, sum;
+      int num1 = 15, num2 = 15, sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
